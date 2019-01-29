29cd8c732038cfae578da3a352ca767a03e735d5
JackSmith <urasovn@mail.ru>
2019-01-29 06:01:08 +0300

    -

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+	"title" => "My first framework",
+	"version" => "0.0.1",
+
+	"db"=>[
+		"dsn"=>"mysql:host=localhost;dbname=skorocake;charset=utf8",
+		"user"=>"root",
+		"password"=>""
+	]
+];
